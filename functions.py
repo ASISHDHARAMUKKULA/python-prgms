21ab821a20fb9db581e1ea6168eaaa47b5d29277
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-06-06 18:54:09 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,3 +8,18 @@ mynum()
 output:
 spam
 eggs
+
+// you must define function before they are called
+
+hello()
+def hello():
+     print("hello")
+        
+output:
+  Traceback (most recent call last):
+  File "A:/python prgms/functions.py", line 1, in <module>
+    hello()
+NameError: name 'hello' is not defined
+
+Process finished with exit code 1  
+            
