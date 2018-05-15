7ee0919f60a1c3f7d1122697d8cf4949d9f5ea55
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2018-05-15 12:40:58 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,12 @@
+names='asish is a good boy'
+print(names.__len__())
+print(names.index('i'))
+print(names.count('o'))
+print(names[0:6])
+#print(names.__reversed__())
+print(names[::-1])
+print(names.upper())
+print(names*3)
+print(names+'sai')
+if 'g' in names:
+    print("it is there")
